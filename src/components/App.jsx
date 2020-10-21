ab0793477adee687116b6b879fdccdc6a21355ef
Zach Chan <zacharyleechan@gmail.com>
2020-10-20 22:01:05 -0700

    implemented search when rendering live data

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,46 +2,75 @@ import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
-import searchYouTube from '../lib/searchYouTube.js';
-
+import Search from './Search.js';
+// import searchYouTube from '../lib/searchYouTube.js';
+var currentList = exampleVideoData;
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
+      youTubeVideos: exampleVideoData,
       currentVideo: exampleVideoData[0],
-      youTubeVideos: exampleVideoData
+      currentSearch: 'cats'
     };
     this.onTitleClick = this.onTitleClick.bind(this);
+    this.clickSearch = this.clickSearch.bind(this);
+    this.userInput = this.userInput.bind(this);
   }
 
   componentDidMount() {
-    searchYouTube({
+    this.props.searchYouTube({
       key: YOUTUBE_API_KEY,
       query: 'cats',
       max: 5
-    }, (data) => this.setState({youTubeVideos: data}));
+    }, (data) => {
+      currentList = data;
+      this.setState({youTubeVideos: data, currentVideo: data[0]});
+    });
   }
 
   onTitleClick(event) {
-    console.log(event.target.id);
     this.setState({
-
-      currentVideo: exampleVideoData[event.target.id]
+      currentVideo: currentList[event.target.id]
     });
-    console.log('button clicked');
   }
 
-  // onSearchClick() {
+  clickSearch() {
+    this.props.searchYouTube({
+      key: YOUTUBE_API_KEY,
+      query: this.state.currentSearch,
+      max: 5
+    }, (data) => {
+      currentList = data;
+      this.setState({youTubeVideos: data, currentVideo: data[0]});
+    });
+  }
 
+  // userInput(event) {
+  //   console.log(event.target.value);
+  //   this.setState({
+  //     currentSearch: event.target.value
+  //   });
   // }
+
+  userInput(event) {
+    this.props.searchYouTube({
+      key: YOUTUBE_API_KEY,
+      query: event.target.value,
+      max: 5
+    }, (data) => {
+      currentList = data;
+      this.setState({youTubeVideos: data, currentVideo: data[0]});
+    });
+  }
 
   render() {
     return (
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <div><h5><em>search</em><Search click= {this.clickSearch} searchText={this.userInput}/></h5></div>
           </div>
         </nav>
         <div className="row">
