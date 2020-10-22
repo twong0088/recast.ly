6bf7e6f8a4d324cba43196dd0522ac477b74f4e2
Terrence Wong <terrencewong223@gmail.com>
2020-10-21 20:21:35 -0700

    added minor changes

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -19,7 +19,3 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
-
-
-
-
